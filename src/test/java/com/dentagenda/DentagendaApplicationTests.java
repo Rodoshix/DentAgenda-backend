76a0cc74ba73aacd1f625b32325f4930e3ef79ec
Rodrigo Sáez <102111944+Rodoshix@users.noreply.github.com>
2025-05-28 16:15:46 -0400

    Add files via upload

---
+++ src/test/java/com/dentagenda/DentagendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentagendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
